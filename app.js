bf6a7d9e0c035e581c6f301db943c3ee75ce151c
naugtur <naugtur@gmail.com>
2024-09-09 01:27:26 +0200

    client recognition maybe5

--- app.js
+++ app.js
@@ -109,6 +109,7 @@ http
     const who = req.headers.authorization;
     if (!who || sha512(who) !== ME) {
       console.error({req, who});
+      res.setHeader("WWW-Authenticate", "Basic realm=who");
       res.statusCode = 417;
       return res.end(`who? ${who}`);
     }
